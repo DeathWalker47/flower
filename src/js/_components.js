e25bca8b38a7b3df284e0168ad93346e7b86b3d0
DeathWalker47 <fizer_00@mail.ru>
2023-10-30 21:16:49 +0300

    Начал делать станицу карточки товара

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@
 import './components/func.js';
 import './components/accardeon.js';
 import './components/map.js';
+import './components/tabs.js';
--- src/js/components/map.js
+++ src/js/components/map.js
@@ -5,22 +5,24 @@ if(breadcrumbCurrent) {
   breadcrumbCurrent.textContent = document.title
 }
 
-ymaps.ready(init);
-function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        center: [56.14339919772827,47.25118528420461],
-        zoom: 15
-    });
-    var myGeoObject = new ymaps.GeoObject({
-      geometry: {
-          type: "Point", // тип геометрии - точка
-          coordinates: [56.14339919772827,47.25118528420461] // координаты точки
-      }
-    });
+if(document.querySelector('.contact-map__map')) {
+  ymaps.ready(init);
+  function init(){
+      // Создание карты.
+      var myMap = new ymaps.Map("map", {
+          center: [56.14339919772827,47.25118528420461],
+          zoom: 15
+      });
+      var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: [56.14339919772827,47.25118528420461] // координаты точки
+        }
+      });
 
-    // Размещение геообъекта на карте.
-    myMap.geoObjects.add(myGeoObject);
+      // Размещение геообъекта на карте.
+      myMap.geoObjects.add(myGeoObject);
+  }
 }
 
 
